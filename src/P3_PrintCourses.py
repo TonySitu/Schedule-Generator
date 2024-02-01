fb5289872040f0c10e3b3ba1b1c2b374bbfc3bba
Tony <137113805+TonySitu@users.noreply.github.com>
2024-02-01 03:27:03 -0500

    Refactored P3

--- src/P3_PrintCourses.py
+++ src/P3_PrintCourses.py
@@ -2,80 +2,88 @@
 # Tony Situ 
 # 2023-06-15
 
-from P2_GenerateSchedules import create_course_dict
+from P2_GenerateSchedules import create_schedules
 from datetime import time, timedelta
 import pickle
 import os
 
-class PrintSchedule:
-    def __init__(self):
-        self._schedule_counter = None
-        self._schedules = self.__get_schedules()
 
-    def __get_schedules(self) -> list:
-        generator = GenerateSchedules()
-        schedules = generator.get_schedules()
-        schedule1, schedule2, schedule3 = schedules[0], schedules[1], schedules[2]
-        return list((schedule1, schedule2, schedule3))
-    
-    def __get_schedule_count(self) -> None:
-        counter_file = "ScheduleCounter.pkl"
-        file_path = os.path.join("data", counter_file)
+def __get_schedules() -> list:
+    schedules, schedule_count = create_schedules()
+    schedule1, schedule2, schedule3 = schedules[0], schedules[1], schedules[2]
+    return list((schedule1, schedule2, schedule3))
 
-        try: 
-            with open(file_path, "rb") as file:
-                self._schedule_counter = pickle.load(file)
-        except:
-            with open(file_path, "wb") as file:
-                self._schedule_counter = 1
-                pickle.dump(self._schedule_counter, file)
-    
-    def print_schedules(self):
-        self.__get_schedule_count()
-        string = ""
-        weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
-        for index in range (3):
-            string += "{0: ^6}|".format("Time")
-            for day_of_week in weekdays:
-                string += "{0: ^21}|".format(day_of_week)
 
-            string += "\n" + "-"*119
+def __get_schedule_count() -> int:
+    counter_file = "ScheduleCounter.pkl"
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    data_dir = os.path.join(current_dir, "..", "data")
+    pkl_dir = os.path.join(data_dir, counter_file)
 
-            for timing in range(8, 21):
-                for increment_time in range(0, 60, 30):
-                    time_block = time(timing, increment_time)
-                    string += "\n\n" + "{0: ^7}|".format(time_block.strftime('%H:%M'))
-                    for weekday in weekdays:
-                        found_course = False
-                        for course in self._schedules[index].get_course_list():
+    try:
+        with open(pkl_dir, "rb") as pkl_file:
+            schedule_counter = pickle.load(pkl_file)
+    except:
+        with open(pkl_dir, "wb") as pkl_file:
+            schedule_counter = 0
+            pickle.dump(schedule_counter, pkl_file)
 
-                            if (course.get_start_time() - timedelta(minutes=5)).time() <= time_block <= course.get_end_time().time() \
+    return schedule_counter
+
+
+def print_schedules():
+    schedule_count = __get_schedule_count()
+    string = ""
+    weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+    schedules = __get_schedules()
+    for index in range(3):
+        string += "{0: ^6}|".format("Time")
+        for day_of_week in weekdays:
+            string += "{0: ^21}|".format(day_of_week)
+
+        string += "\n" + "-" * 119
+
+        for timing in range(8, 21):
+            for increment_time in range(0, 60, 30):
+                time_block = time(timing, increment_time)
+                string += "\n\n" + "{0: ^7}|".format(time_block.strftime('%H:%M'))
+                for weekday in weekdays:
+                    found_course = False
+                    for course in schedules[index].get_course_list():
+
+                        if (course.get_start_time() - timedelta(
+                                minutes=5)).time() <= time_block <= course.get_end_time().time() \
                                 and weekday in course.get_days():
-                                string += "{0: ^21}|".format(course.get_code() + course.get_section())
-                                found_course = True
+                            string += "{0: ^21}|".format(course.get_code() + course.get_section())
+                            found_course = True
 
-                        if found_course is False:
-                            string += "{0: ^21}|".format("")
-            string += f"\n\n{self._schedules[index]}"
-            string += "Course Codes: {}\n".format(", ".join(code for code in self._schedules[index].get_code_list()))
-            string += "Crn List: {}\n\n".format(", ".join(str(crn) for crn in self._schedules[index].get_crn()))
-            print(string, end="")
+                    if found_course is False:
+                        string += "{0: ^21}|".format("")
+        string += f"\n\n{schedules[index]}"
+        string += "Course Codes: {}\n".format(", ".join(code for code in schedules[index].get_code_list()))
+        string += "Crn List: {}\n\n".format(", ".join(str(crn) for crn in schedules[index].get_crn()))
+        print(string, end="")
 
-        output_file = "Schedule_" + str(self._schedule_counter) + ".txt"
+    schedule_count += 1
+    output_file = "Schedule_" + str(schedule_count) + ".txt"
+    print(output_file)
+    counter_file = "ScheduleCounter.pkl"
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    data_dir = os.path.join(current_dir, "..", "data")
+    pkl_dir = os.path.join(data_dir, counter_file)
 
-        self._schedule_counter += 1
-        counter_file = "ScheduleCounter.pkl"
-        file_path = os.path.join("data", counter_file)
+    with open(pkl_dir, "wb") as file:
+        pickle.dump(schedule_count, file)
 
-        with open(file_path, "wb") as file:
-                pickle.dump(self._schedule_counter, file)
+    generated_schedules_dir = os.path.join(current_dir, "..", "GeneratedSchedules")
+    output_file_dir = os.path.join(generated_schedules_dir, output_file)
+    with open(output_file_dir, "w") as output_file:
+        output_file.write(string)
 
-        file_path = os.path.join("GeneratedSchedules", output_file)
-        with open(file_path, "w") as file:
-            file.write(string)
 
 def main():
-    PrintSchedule().print_schedules()
+    print_schedules()
+
 
 if __name__ == "__main__":
     main()
